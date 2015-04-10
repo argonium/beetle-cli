af2e3faa8fac55449a2bef3ca541a04211ca5f9a
Wallace, Michael <MWallace@eab.com>
2015-04-10 07:56:08 -0400

    Implemented the method to write the string buffer to disk.

--- src/io/miti/beetle/exporters/DBFileWriter.java
+++ src/io/miti/beetle/exporters/DBFileWriter.java
@@ -1,6 +1,11 @@
 package io.miti.beetle.exporters;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 
@@ -16,6 +21,12 @@ public abstract class DBFileWriter
     super();
   }
   
+  /**
+   * Constructor taking the output filename and result set.
+   * 
+   * @param sFilename the output filename
+   * @param pRSMD the database result set metadata (column names, etc.)
+   */
   public DBFileWriter(final String sFilename, final ResultSetMetaData pRSMD) {
     sb = new StringBuilder(100);
     file = new File(sFilename);
@@ -23,17 +34,51 @@ public abstract class DBFileWriter
     rsmd = pRSMD;
   }
   
+  /**
+   * Abstract method for writing the file's header.
+   */
   public abstract void writeHeader();
   
+  /**
+   * Abstract method for writing the file's footer.
+   */
   public abstract void writeFooter();
   
+  /**
+   * Abstract method for writing a database result set row.
+   * 
+   * @param rsj the result set
+   */
   public abstract void writeObject(final ResultSet rsj);
   
+  /**
+   * If the string buffer is big enough, flush it to disk.
+   */
   public final void writeString() {
     // Check the length of the string
     if (sb.length() > 10000) {
-      // TODO Write the string to a file
       
+    	// The input buffer is long enough that we need to
+    	// write it to file and then clear out the buffer
+    	PrintWriter filePw = null;
+		try {
+			// Open the file for appending
+			filePw = new PrintWriter(new OutputStreamWriter(new FileOutputStream(
+			        filename, true), "UTF-8"));
+
+	        filePw.print(sb.toString());
+	        
+			filePw.close();
+			filePw = null;
+		} catch (UnsupportedEncodingException e) {
+		} catch (FileNotFoundException e) {
+		} finally {
+			if (filePw != null) {
+			// Close the writer
+			filePw.close();
+			}
+		}
+
       sb.setLength(0);
     }
   }
