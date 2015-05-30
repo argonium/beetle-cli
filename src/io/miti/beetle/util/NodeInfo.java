003d1ed82f68120303ed309cfd8f3f66fdbcdede
Mike Wallace <mfwallace@gmail.com>
2015-05-29 21:10:40 -0400

    Initial release.

---
+++ src/io/miti/beetle/util/NodeInfo.java
@@ -0,0 +1,32 @@
+package io.miti.beetle.util;
+
+public final class NodeInfo
+{
+  private String name = null;
+  private Class<?> clazz = null;
+  
+  public NodeInfo() {
+    super();
+  }
+  
+  public NodeInfo(final String sName, final Class<?> pClazz) {
+    name = sName;
+    clazz = pClazz;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Class<?> getClazz() {
+    return clazz;
+  }
+
+  public void setClazz(Class<?> clazz) {
+    this.clazz = clazz;
+  }
+}
